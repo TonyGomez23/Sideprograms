23f7949c773d6fe136d76f04a104467e285c67d8
TonyGomez23 <72.tonygomez@gmail.com>
2019-07-12 09:22:54 -0400

    Practice

---
+++ Practice.js
@@ -0,0 +1,17 @@
+let king = ["George", "Mark", "Maxis"];
+let jack = ["B1", "B2", "B3", "B4"];
+let queen = ['J1', 'J2', 'J3', 'J4'];
+
+if (jack = "J1" || "J2" || "J3" || "J4"){
+    console.log('Welcome to blackjack');
+} else {
+    console.log('Who are you?');
+}
+
+let pokemon = process.argv[2];
+
+if (pokemon = undefined){
+    console.log("Please put in a pokemon");
+}else{
+    console.log("I see you're a man of culture");
+}
